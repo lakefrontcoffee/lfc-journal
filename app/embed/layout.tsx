32b7e6eb833f1dec8132731fb15cdd2b879faae4
lakefrontcoffee <DRKONGRKL@GMAIL.COM>
2025-10-24 16:48:08 -0500

    Create layout.tsx

---
+++ app/embed/layout.tsx
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: 'Lakefront Embed',
+  description: 'Embedded Lakefront Journal routes',
+};
+
+export default function EmbedLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return <>{children}</>;
+}
