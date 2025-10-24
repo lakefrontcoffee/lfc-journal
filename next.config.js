69888cc11abbc4e59219f026721ebd89c2eeef04
lakefrontcoffee <DRKONGRKL@GMAIL.COM>
2025-10-24 17:09:29 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  webpack: (config) => {
+    // Prevents "Can't resolve 'encoding'" & "pino-pretty" build warnings
+    config.resolve.fallback = {
+      ...(config.resolve.fallback || {}),
+      encoding: false,
+      'pino-pretty': false,
+    };
+    return config;
+  },
 };
 
 module.exports = nextConfig;
