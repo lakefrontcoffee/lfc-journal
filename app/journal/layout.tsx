6445dae50810a6d4187a910b6f4e9b3107dfd2dc
lakefrontcoffee <DRKONGRKL@GMAIL.COM>
2025-11-16 01:12:45 -0600

    Update layout.tsx

--- app/journal/layout.tsx
+++ app/journal/layout.tsx
@@ -1,13 +1,12 @@
-// app/embed/layout.tsx
+// app/journal/layout.tsx
 export const dynamic = 'force-dynamic';
 export const revalidate = 0;
 export const fetchCache = 'default-no-store';
 
-export default function EmbedLayout({
+export default function JournalLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return <section>{children}</section>;
 }
-
